24bcc655c276f892e5d931f0cb36a98c04076526
Godswillomuka <omukagodswil@gmail.com>
2024-11-04 12:48:12 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About (){
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 
 function App() {
@@ -24,8 +31,11 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
     </div>
+
   );
 }
 
